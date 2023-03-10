171005c679eb9ba860993191476e4c0b796c4282
VictorLCosta <victorlc2019@outlook.com>
2023-03-10 13:14:20 -0300

    feat(api): adds exception middleware for exception handling

--- api/Web/Program.cs
+++ api/Web/Program.cs
@@ -1,4 +1,5 @@
 using Api.Infrastructure.Persistence;
+using Api.Web.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,6 +24,8 @@ if (app.Environment.IsDevelopment())
         await initialiser.SeedAsync();
     }
 }
+
+app.UseMiddleware<ExceptionMiddleware>();
 
 app.UseRouting();
 
