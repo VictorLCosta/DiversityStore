005c49c0cfb82fef71f2d3cca571ed142afe36d8
VictorLCosta <victorlc2019@outlook.com>
2023-03-10 16:09:01 -0300

    chore(api): adds FluentValidation package

--- api/Application/ConfigureServices.cs
+++ api/Application/ConfigureServices.cs
@@ -1,7 +1,8 @@
 using System.Reflection;
 
 using Api.Application.Common.Behaviours;
-using Api.Application.Products.Queries.GetProducts;
+
+using FluentValidation;
 
 using MediatR;
 
@@ -12,6 +13,7 @@ public static class ConfigureServices
     public static IServiceCollection AddApplicationServices(this IServiceCollection services)
     {
         services.AddAutoMapper(Assembly.GetExecutingAssembly());
+        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
         services.AddMediatR(cfg => {
             cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly());
             cfg.AddBehavior(typeof(IPipelineBehavior<,>), typeof(PerformanceBehaviour<,>));
